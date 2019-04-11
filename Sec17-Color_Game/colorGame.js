1b824f9d42fa293e3cc54588b3094b3f43091e3b
Anna Sun <annasun.usa@gmail.com>
2019-04-11 22:25:09 +0800

    Added DOM, CSS, and Animation

--- Sec17-Color_Game/colorGame.js
+++ Sec17-Color_Game/colorGame.js
@@ -1,1 +1,124 @@
 
+// var colors = [
+// 	"rgb(255, 0, 0)",
+// 	"rgb(255, 255, 0)",
+// 	"rgb(0, 255, 0)",
+// 	"rgb(0, 255, 255)",
+// 	"rgb(0 0, 255)",
+// 	"rgb(255, 0, 255)",
+// ]
+var colors = [];
+
+var numSquares = 6;
+var pickedColor = pickColor();
+var clickedColor;
+
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+
+
+colorDisplay.textContent = pickedColor;
+setupModeButtons();
+setupSquares();
+reset();
+
+resetButton.addEventListener("click", function(){
+	reset();
+})
+
+function setupModeButtons(){
+	for (var i = 0; i < modeButtons.length; i++){
+		modeButtons[i].addEventListener("click", function(){
+
+			// Remove .selected class from both Easy and Hard mode buttons, and add .selected class to the mode button selected.  So the right button is highlighted.
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+			console.log("ln-43, squares.length:", squares.length);
+			reset();
+		});
+	}
+}
+
+function setupSquares() {
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.background = colors[i];
+
+		squares[i].addEventListener("click", function() {
+			var clickedColor = this.style.background;
+			if (clickedColor === pickedColor) {
+				messageDisplay.textContent = "Correct!";
+				changeColors(clickedColor);
+				h1.style.background = clickedColor;
+				resetButton.textContent = "Play Again?"
+			} else {
+				this.style.backgroundColor = "#232323";
+				messageDisplay.textContent = "Try Again";
+			}
+		});
+	}
+}
+
+function reset(){
+	colors = generateRandomColors(numSquares);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked Color
+	colorDisplay.textContent = pickedColor;
+	resetButton.textContent = "New Colors"
+	messageDisplay.textContent = "";
+
+	for (var i = 0; i < squares.length; i++){
+		console.log("squares.length:", squares.length);
+
+		// Hard mode: If there is color to paint, display it.  Bring back the top 3 squares when change from easy to hard mode
+		if (colors[i]){
+			console.log("if, i: ", i);
+			squares[i].style.display = "block";
+			//console.log("i: ", i);
+			squares[i].style.background = colors[i];
+		}
+		// Easy mode:  colors[] only has the first 3 indices available with colors. So color[3~5] has no val's and goes to "else" here.
+		else {
+			console.log("else, i: ", i);
+			// Set squares[i] element to not be displayed
+			squares[i].style.display = "none";
+		}
+	}
+	h1.style.background = "steelblue";
+}
+
+function changeColors(color) {
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num){
+	var arr = []
+	for(var i = 0; i < num; i++){
+		arr.push(randomColor())
+	}
+	return arr;
+}
+
+function randomColor(){
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	//pick a "green" from  0 -255
+	var g = Math.floor(Math.random() * 256);
+	//pick a "blue" from  0 -255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
