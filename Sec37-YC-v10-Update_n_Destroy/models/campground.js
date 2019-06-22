df27b03778a810501a9b3764e2d5a76b19ebbaf8
Anna Sun <annasun.usa@gmail.com>
2019-06-23 01:57:34 +0800

    Cleaned up code

--- Sec37-YC-v10-Update_n_Destroy/models/campground.js
+++ Sec37-YC-v10-Update_n_Destroy/models/campground.js
@@ -1,15 +1,10 @@
 var mongoose = require("mongoose");
 
-// SCHEMA SETUP
 var CampgroundSchema = new mongoose.Schema({
   name: String,
   image: String,
   description: String,
 
-  /*
-  Data Association: 
-  - Save id+username to newly created campground, so a user only needs to enter a new campground, and the website knows whom the user is.
-  */
   author: {
     id: {
       type: mongoose.Schema.Types.ObjectId,
