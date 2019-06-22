85c09b6d01737b8a6df735372e95434f00f90814
Anna Sun <annasun.usa@gmail.com>
2019-06-23 01:54:44 +0800

    Cleaned up code

--- Sec36-YC-v8-Data_Associations/app.js
+++ Sec36-YC-v8-Data_Associations/app.js
@@ -9,7 +9,6 @@ var express     = require("express"),
     LocalStrategy = require('passport-local'),
     User        = require("./models/user");
 
-// Requring my modules that'll be used as Express Router middleware in app.use() below
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
@@ -37,9 +36,6 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());     
 passport.deserializeUser(User.deserializeUser()); 
 
-//---------------------------------------------------------
-// Make req.user available in all (views) templates by adding a new var in res.locals
-// (Notice: re"q".user and re"s".locals.currentUser)
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
    next();
@@ -48,26 +44,11 @@ app.use(function(req, res, next){
 //---------------------------------------------------------
 // Express Router Config
 //---------------------------------------------------------
-/*
-app.use(): 
-- Bind application-level middleware to an instance of the app object
-
-ex.
-app.use("/", indexRoutes);
-- indexRoutes is a middleware function mounted on the "/" path. 
-  (indexRoutes was required from routes/index.js)
-- The function is executed for any type of HTTP request on the path.
-
-Note: 
-- See "var router  = express.Router();" in index.js to know why indexRoutes 
-  is a middleware func.
-*/
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/", indexRoutes);  // same as: app.use(indexRoutes);
 
-//---------------------------------------------------------
-// Middleware
+
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()){
         return next();
@@ -75,10 +56,6 @@ function isLoggedIn(req, res, next) {
     res.redirect("/login");
 };
 
-// For running on cloud9:
-//app.listen(process.env.PORT, process.env.IP, function(err, req) {
-//
-// For running on localhost:
 app.listen("8080", "localhost", function(err) {
    console.log("The YelpCamp Server Has Started!");
 });
